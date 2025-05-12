cc72b1f14af136a084bbfc771901f1066259dc3e
Kai Wui Ye <kye5595@gmail.com>
2025-05-12 16:20:32 -0400

    page now resets when clicking a new show

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 /**
- * React TV is an web streaming platform that allows users to browse
- * through the episodes of a variety of different shows.
+ * React TV is an web streaming platform that allows users to browse through the episodes of a variety of different shows.
  */
+
+import { useState } from "react";
+import ShowDetails from "./shows/ShowDetails";
+import ShowSelection from "./shows/ShowSelection";
+import { tvShows } from "./shows/data";
+
 export default function App() {
+  const [selectedShow, setSelectedShow] = useState(null);
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+
   return (
     <>
       <header>
         <p>React TV</p>
+        <ShowSelection
+          shows={tvShows}
+          setSelectedShow={(show) => {
+            setSelectedShow(show);
+            setSelectedEpisode(null);
+          }}
+        />
       </header>
-      <main></main>
+      <main>
+        <ShowDetails
+          show={selectedShow}
+          selectedEpisode={selectedEpisode}
+          setSelectedEpisode={setSelectedEpisode}
+        />
+      </main>
     </>
   );
 }
--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,26 @@
 import "./shows.css";
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({
+  show,
+  selectedEpisode,
+  setSelectedEpisode,
+}) {
+  if (!show) {
+    return <div>Please select a show to learn more!</div>;
+  }
+
+  return (
+    <div className="show-details">
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
+  );
 }
